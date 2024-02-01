f2ee8ebca82c19bb3070105a59f45c99176b3a41
rdx_coder <rahul.dangi.sait@gmail.com>
2024-02-01 16:49:53 +0530

    create a expense app 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import React, { useReducer } from "react";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
 import ExpenseList from "./components/ExpenseList/ExpenseList";
@@ -23,16 +23,26 @@ function App() {
   // Use useReducer hook to manage state
   const [expenses, dispatch] = useReducer(expensesReducer, initialState);
 
+  // Function to add a new expense
+  const addExpense = (newExpense) => {
+    dispatch({ type: "ADD_EXPENSE", payload: newExpense });
+  };
+
+  // Function to delete an expense
+  const deleteExpense = (id) => {
+    dispatch({ type: "DELETE_EXPENSE", payload: id });
+  };
+
   return (
     <>
       <h2 className="mainHeading">Expense Tracker</h2>
       <div className="App">
-        {/* Pass dispatch function to ExpenseForm to handle adding expenses */}
-        <ExpenseForm addExpense={(expense) => dispatch({ type: "ADD_EXPENSE", payload: expense })} />
+        {/* Pass addExpense function to ExpenseForm */}
+        <ExpenseForm addExpense={addExpense} />
         <div className="expenseContainer">
           <ExpenseInfo expenses={expenses} />
-          {/* Pass dispatch function to ExpenseList to handle deleting expenses */}
-          <ExpenseList expenses={expenses} deleteExpense={(id) => dispatch({ type: "DELETE_EXPENSE", payload: id })} />
+          {/* Pass deleteExpense function to ExpenseList */}
+          <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
         </div>
       </div>
     </>
